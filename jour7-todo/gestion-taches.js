1801d9c6b783de838955c104933a2b5fe0d442de
sapey <alexandre.froment92@gmail.com>
2021-12-09 15:17:46 +0100

    end exo todo

--- jour7-todo/gestion-taches.js
+++ jour7-todo/gestion-taches.js
@@ -1,27 +1,41 @@
 window.addEventListener("DOMContentLoaded"  , async() => {
 
     const reponse = await fetch("http://localhost:3000/todos" )
-
     const todos = await reponse.json(); 
+    
     document.querySelector(".js-list-tache").innerHTML = genererFormsTaches(todos);
-    document.querySelector(".js-list-tache").addEventListener("click" , e => {
+
+    // gestion du nombre de tâches en cours 
+    document.querySelector(".js-compteur").innerHTML = todos.filter( todo => todo.status === false ).length; 
+
+
+    // écouter quand on clique dans la zone js-list-tache
+    document.querySelector(".js-list-tache").addEventListener("click" , async e => {
         e.preventDefault();
         if(e.target.className.includes("btn")){
             const form = e.target.parentNode;
-            const data = {
-                id : form.id.value,
-                name : form.name.value,
-                status : form.status.value == "0" ? false : true
+            const action = e.target.value ;
+            const id = form.id.value
+            if(action == "modifier"){
+                const data = {
+                    id : id,
+                    name : form.name.value,
+                    status : form.status.value == "0" ? false : true
+                }
+                const options = { method : "PUT" , body : JSON.stringify(data) , headers : {'Content-Type': 'application/json'} }
+                await fetch("http://localhost:3000/todos/"+id , options)
+            }else if(action == "supprimer"){
+                const options = {method : "DELETE"}
+                await fetch("http://localhost:3000/todos/"+id , options);
             }
-            console.log(data);
-
-            // PUT => update
-            // DELETE => suppr
         }
     })
 })
 
 function genererFormsTaches(data){
+
+    if(data.length === 0) return "<p>Veuillez ajouter des tâches</p>";
+
     return data.map( d => {
         return `<form class="d-flex my-3">
         <input type="hidden" name="id" class="form-input" value="${d.id}">
